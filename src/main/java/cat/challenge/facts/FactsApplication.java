af099251da0a4e6acbe1f2e529d08db3d9000fcb
RobM <sohrab.makaremi@gmail.com>
2023-03-10 16:11:48 +1000

    Initial commit

---
+++ src/main/java/cat/challenge/facts/FactsApplication.java
@@ -0,0 +1,13 @@
+package cat.challenge.facts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/challenge/facts/FactsApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.challenge.facts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
